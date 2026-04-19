923ccde0e68701f780a6eab416bd7171188629d8
lgkpba <lgkpba@gmail.com>
2026-04-19 15:16:20 -0300

    mudei de recursão para iteração

--- binarysearch.c
+++ binarysearch.c
@@ -1,39 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int busca_binaria(int vetor[], int num_procurado, int comeco, int fim){
-    int meio = (comeco + fim) / 2;
-    if(vetor[meio] == num_procurado){
-        return meio;
-    }
+int busca_binaria(int vetor[], int num_procurado, int tamanho){
+    int comeco = 0;
+    int fim = tamanho - 1;
+    int meio;
     
-    if(comeco >= fim){
-        return -1;
-    }
-    else{
-        if(vetor[meio] < num_procurado){
-            return busca_binaria(vetor, num_procurado, meio + 1, fim);
+    while(comeco <= fim){
+        meio = (comeco + fim) / 2;
+        if(vetor[meio] == num_procurado){
+            while(meio > 0 && vetor[meio - 1] == num_procurado){
+                meio--;
+            }
+            return meio;
         }
-        else{
-            return busca_binaria(vetor, num_procurado, comeco, meio - 1);
+        if(vetor[meio] > num_procurado){
+            fim = meio - 1;
+        } else{
+            comeco = meio + 1;
         }
     }
+    return -1;
 }
 
 int main(void){
     int tam_vetor, num_buscas, num_procurado;
     scanf("%d %d", &tam_vetor, &num_buscas);
+    
     int *vetor = malloc(tam_vetor * sizeof(int));
-
     for(int i = 0; i < tam_vetor; i++){
         scanf("%d", &vetor[i]);
     }
-
+    
     for(int j = 0; j < num_buscas; j++){
         scanf("%d", &num_procurado);
-        int indice = busca_binaria(vetor, num_procurado, 0, tam_vetor - 1);
+        int indice = busca_binaria(vetor, num_procurado, tam_vetor);
         printf("%d\n", indice);
     }
+    
     free(vetor);
     return 0;
 }
